55af0985b75fa0bdd375bec5659519a678f93ea4
Azcion <zak.klajderic@gmail.com>
2016-06-23 02:32:13 +0200

    Fixed large memory footprint with Ghost object

---
+++ Ghost.cs
@@ -0,0 +1,23 @@
+﻿namespace ComplexLifeforms {
+
+	public struct Ghost {
+
+		public readonly ushort Age;
+
+		public readonly byte Urge;
+		public readonly byte Emotion;
+		public readonly byte Mood;
+		public readonly byte DeathBy;
+
+		public Ghost (Lifeform lifeform) {
+			Age = (ushort) lifeform.Age;
+
+			Urge = (byte) lifeform.Mood.Urge;
+			Emotion = (byte) lifeform.Mood.Emotion;
+			Mood = (byte) lifeform.Mood.Mood;
+			DeathBy = (byte) lifeform.DeathBy;
+		}
+
+	}
+
+}
