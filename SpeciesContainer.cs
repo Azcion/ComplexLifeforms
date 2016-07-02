43cb4a414ee5f781c140ce4f95bb1aad5b28f589
Azcion <zak.klajderic@gmail.com>
2016-07-02 02:17:10 +0200

    Lowered foraging chances for Beta species

--- SpeciesContainer.cs
+++ SpeciesContainer.cs
@@ -30,6 +30,7 @@ namespace ComplexLifeforms {
 		private static readonly int[][] CHANCES = {
 				//     eat       drink
 				new[] {2, 5, 10, 2, 1, 10},
+				new[] {5, 4,  8, 5, 1,  5},
 				new[] {1, 5, 10, 1, 5, 10}
 		};
 
